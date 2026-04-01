a9642d3d52bd00a5e82c78202aac494eb95f46b3
Code Baithak <vohrasamarth.cb@gmail.com>
2026-04-01 17:00:11 +0530

    baithak 13 code JS variables/datatypes

---
+++ Baithak-13/variables/app.js
@@ -0,0 +1,39 @@
+
+// var a = 500;
+// console.log(a);
+// console.log(10);
+
+
+// ------------------------------
+
+// naamkaran
+
+// let codebaithak = 10;
+// console.log(codebaithak);
+
+// let codebaithak123 = true;
+// console.log(codebaithak123);
+
+// galti na kre ❌
+// let 12codebaithak = true;
+// console.log(12codebaithak);
+
+// let code_baithak = "Best"
+// console.log(code_baithak);
+
+
+// let $code_baithak = "Bestest"
+// console.log($code_baithak);
+
+
+// galti na kre ❌
+// let let = 10
+// console.log(let);
+
+// fun element
+let _ = 1;
+console.log(_);
+
+
+let $ = true;
+console.log($);
