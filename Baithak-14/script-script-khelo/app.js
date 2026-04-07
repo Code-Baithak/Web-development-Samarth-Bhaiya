d8489f8b85ed390ebae931a6b34e5f579eed5282
Code Baithak <vohrasamarth.cb@gmail.com>
2026-04-08 00:54:31 +0530

    Baithak 14 code

---
+++ Baithak-14/script-script-khelo/app.js
@@ -0,0 +1,9 @@
+// debugger
+console.log("samarth vohra");
+console.log("code_bithak");
+
+let bunny = "pink"
+console.log(bunny);
+
+
+
---
+++ Baithak-14/variables/app.js
@@ -0,0 +1,44 @@
+// var - ES5
+// var a = 10;
+// console.log(a); //10
+// var a = "sam"; //redeclare ✅
+// console.log(a); //sam
+// a = true; //reassign ✅
+// console.log(a); //true
+
+// ------------------------
+
+// let & const - ES6
+
+// let x = 10;
+// console.log(x);
+// // let x = true; //redeclare ❌
+// // console.log(x);
+
+// x = true; //reassign ✅
+// console.log(x);
+
+// ------------------------
+
+// const
+// const z = 1;
+// console.log(z);
+// // const z = true; //redeclare ❌
+// // console.log(z);
+// z = 100; //reassign ❌
+// console.log(z);
+
+// ------------------------
+
+// const x;
+// x = 10;
+
+const x = 100;
+
+
+
+
+
+
+
+
