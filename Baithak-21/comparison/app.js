db77503f266cacf063c525e8f1dfeb3c1b34b0d8
Code Baithak <vohrasamarth.cb@gmail.com>
2026-06-14 18:35:42 +0530

    baithak 21 codes

---
+++ Baithak-21/comparison/app.js
@@ -0,0 +1,44 @@
+
+
+// let a = 10;
+// let b = 20;
+
+
+// console.log( a > b ); // 10 > 20 // false
+// console.log( a < b ); // 10 < 20 // true
+
+
+// let a = 10;
+// let b = 10;
+
+// console.log( a <= b ); // true
+// console.log( a >= b ); // true
+
+// -----------------------------------
+
+// let a = 10;
+// let b = 10
+
+
+// console.log( a == b );
+
+// let a = 10;
+// let b = "10";
+
+// console.log( a == b );
+
+
+// -----------------------------------
+
+// let a = 10;
+// let b = 10;
+
+// let a = 10;
+// let b = "10";
+
+// console.log( a === b );
+// -----------------------------------
+
+
+
+
