9e5b1bbe8a9b65d603c8fe82e87b5b02773e658f
Code Baithak <vohrasamarth.cb@gmail.com>
2026-06-14 18:34:54 +0530

    baithak 18 codes

---
+++ Baithak-18/alert_confirm/app.js
@@ -0,0 +1,15 @@
+
+
+// alert("Login successfully done")
+
+
+
+// confirm("Are you sure you want to login?")
+
+let returnedVal = confirm("are you 18+ ?")
+
+if(returnedVal){
+    console.log("yeah enter the club... ❤️");
+}else{
+    console.log("You are not allowed ❌");
+}
---
+++ Baithak-18/prompt/app.js
@@ -0,0 +1,28 @@
+
+let userInput = prompt("Hey please enter your age...") //string return
+
+console.log(userInput); //STRING
+
+// if(Number(userInput)>18){
+//     console.log("ADULT");
+// }else{
+//     console.log("KID");
+// }
+
+//correct case 
+if(Number(userInput) === 18){
+    console.log("ADULT");
+}else{
+    console.log("KID");
+}
+
+
+//prev case
+if(userInput === 18){
+    console.log("ADULT");
+}else{
+    console.log("KID");
+}
+
+
+
