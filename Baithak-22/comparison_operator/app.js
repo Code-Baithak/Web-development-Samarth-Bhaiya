288e16a096d8a1f180567500c72fcba765a817a4
Code Baithak <vohrasamarth.cb@gmail.com>
2026-06-14 18:35:57 +0530

    baithak 22 codes

---
+++ Baithak-22/comparison_operator/app.js
@@ -0,0 +1,63 @@
+
+
+// console.log( 5>3 && 3<4 );
+// t && t
+// t
+
+
+// console.log( 5<3 && 3<4 );
+//f 
+
+
+// console.log( 10 == "10" || 10 === "10" );
+// console.log( 10 == "5" || 10 === "10" );
+
+// t || false
+// true
+
+// ----------------------------
+
+// let marks = 33;
+// let attendance = 75;
+
+
+// console.log( marks >= 40 || attendance >= 75 );
+
+// // f || t
+// // t
+// ----------------------------
+
+// let marks = 33;
+// let attendance = 75;
+
+
+// console.log( marks >= 40 && attendance >= 75 );
+
+// f
+// ----------------------------
+
+
+// let marks = 33;
+// // let attendance = 75;
+
+
+// console.log( marks >= 40 && attendance >= 75 ); //short circuiting
+// //f
+
+// ----------------------------
+
+
+// let marks = 53;
+// // let attendance = 75;
+
+
+// console.log( marks >= 40 || attendance >= 75 ); //short circuiting
+// // t
+
+
+// console.log( !true );
+// console.log( !false );
+
+
+
+
---
+++ Baithak-22/if-else/app.js
@@ -0,0 +1,59 @@
+
+// let a = true;
+// let a = false;
+
+// if(true){}
+// if(a){
+//     console.log("welcome to code_baithak");
+// }
+// ---------------------
+
+// if(10>10){
+//     console.log("balle balle");
+// }else{
+//     console.log("mai else hu hahaha");
+// }
+
+
+// ---------------------
+
+// if(10>=10){
+//     console.log("balle balle");
+// }else{
+//     console.log("mai else hu hahaha");
+// }
+
+// ---------------------
+
+// if(){}
+// else if(){}
+// else if(){}
+// else if(){}
+// else{}
+
+
+// ---------------------
+
+
+let marks = 43;
+
+if(marks >= 90){
+    console.log("A");
+}
+else if(marks>=80){
+    console.log("B");
+}
+else if(marks>=70){
+    console.log("C");
+}
+else if(marks>=60){
+    console.log("D");
+}
+else if(marks>=50){
+    console.log("E");
+}
+else{
+    console.log("fail");
+}
+
+
