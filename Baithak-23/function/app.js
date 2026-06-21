2373bca75d16af348dac38787f6b93e50118bdbe
Code Baithak <vohrasamarth.cb@gmail.com>
2026-06-21 17:58:31 +0530

    baithak 23 codes

---
+++ Baithak-23/function/app.js
@@ -0,0 +1,91 @@
+
+// function kuchbhi(){
+//     return true
+// }
+// let returnedVal = kuchbhi(); //returned value yahan aegi
+// console.log(returnedVal);
+// console.log(kuchbhi());
+
+
+// nature of the fn is to return something
+
+// if nothing is returned then by default undefined is returned
+
+// -----------------------------------
+
+// function sam(){
+//     console.log("hello from sam");
+//     console.log("balle balle")
+// }
+
+// sam()
+// sam()
+// sam()
+
+// -----------------------------------
+//paramterised fn
+// function sam(x,y,z){ // parameters
+//     console.log(x , y , z)
+//     // console.log(y)
+//     // console.log(z)
+// }
+
+// sam(10,20,"sam") // arguments
+
+// -----------------------------------
+
+// function sam(x,y,z){ 
+//     console.log(x , y , z)
+
+// }
+
+// sam(10,20)
+
+// ----------------------------------
+// function sam(x,y,z=true){ 
+//     console.log(x , y , z)
+// }
+
+// sam(10,20)
+
+
+// ----------------------------------
+// function sam(x=1,y=3,z=true){ 
+//     console.log(x , y , z)
+// }
+
+// sam(10,20,false) //arguments
+
+
+// ----------------------------------
+
+
+// function sam(x=1,y=3,z=true){ 
+//     return x+y
+// }
+
+// let retVal = sam(10,20,false) //arguments
+// console.log(retVal);
+
+
+// ----------------------------------
+
+
+// function sam(x=1,y=3,z=true){ 
+//     return x+y+z
+// }
+
+// let retVal = sam(10,20,false) //arguments
+// console.log(retVal);
+
+
+// ----------------------------------
+
+
+// function sam(x=1,y=3,z=true){ 
+//     return x+y+z
+// }
+
+// let retVal = sam(10,20) //arguments
+// console.log(retVal);
+
