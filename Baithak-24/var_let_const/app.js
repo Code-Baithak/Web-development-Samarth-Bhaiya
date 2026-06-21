fa26799805c866bdc31059e726792902616b2856
Code Baithak <vohrasamarth.cb@gmail.com>
2026-06-21 18:24:30 +0530

    baithak 24 codes

---
+++ Baithak-24/var_let_const/app.js
@@ -0,0 +1,97 @@
+//redeclare
+
+// var a = 10;
+// console.log(a);
+
+// var a = 2000;
+// console.log(a);
+
+// -------------------
+//reassign
+
+// var a = 10;
+// console.log(a);
+
+// a = true;
+// console.log(a);
+
+
+// --------------------------------------
+// reassign
+
+// let a = 10;
+// console.log(a);
+// a = "balle balle" //reassign
+// console.log(a);
+
+// ------------------
+
+// redeclare
+
+// let a = 10;
+// console.log(a);
+// let a = "balle balle" //redeclare
+// console.log(a);
+
+
+// --------------------------------------
+
+// const x = 10;
+// console.log(x);
+
+// x = true; //reassign
+// console.log(x);
+
+// ----------------------
+
+// const x = 10;
+// console.log(x);
+
+// const x = true; //redecalre
+// console.log(x);
+
+
+// --------------------------------------
+
+
+// var a = 10;
+// console.log(a); // 10
+// var a  = 200;
+// console.log(a) // 200
+
+
+// var a = 10;
+// console.log(a); // 10
+// a  = 200;
+// console.log(a) // 200
+
+
+
+// let a = 10;
+// console.log(a); // 10
+// a  = 200;
+// console.log(a) // 200
+
+
+// let a = 10;
+// console.log(a); // 10
+// let a  = 200;
+// console.log(a) // 200
+
+
+// var a;
+// console.log(a); 
+// a = 400
+// console.log(a); 
+
+
+// let b;
+// console.log(b); 
+// b = 400
+// console.log(b); 
+
+
+const b = 100;
+console.log(b); 
+b = 400
+console.log(b); 
