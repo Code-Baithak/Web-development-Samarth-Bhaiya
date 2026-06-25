ee53c614fb289b98382bdd985d63ee1bba219cc2
Code Baithak <vohrasamarth.cb@gmail.com>
2026-06-25 19:12:44 +0530

    baithak 25 codes

---
+++ Baithak-25/secret/app.js
@@ -0,0 +1,46 @@
+
+
+// var a = 10;
+// console.log(a);
+
+// --------------------
+
+// var a = 10;
+
+// console.log(a);
+
+
+// function abc(){
+//     console.log("code baithak ki duniya");
+// }
+
+// abc()
+
+
+// --------------------
+
+// console.log(a);
+// var a = 10;
+// var b = 20;
+// console.log(b);
+
+// abc()
+// function abc(){
+//     console.log(c);
+//     var c = true;
+//     console.log(c);
+// }
+
+// --------------------
+
+// hoisting
+// console.log(a); //access before
+// var a = 10;
+
+// -------
+
+
+console.log(a); //access before
+// let a = 10;
+const a = 10;
+
